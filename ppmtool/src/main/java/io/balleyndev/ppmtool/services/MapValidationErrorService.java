ca650b3220aa747c0b6565fa3e61eec5f567b44f
BAlleyneUWO <balleyn@uwo.ca>
2020-03-23 19:14:59 -0400

    Add simple controller and error logic

---
+++ ppmtool/src/main/java/io/balleyndev/ppmtool/services/MapValidationErrorService.java
@@ -0,0 +1,30 @@
+package io.balleyndev.ppmtool.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+@Service
+public class MapValidationErrorService {
+      
+	public ResponseEntity<?> MapValidationService(BindingResult result){
+        
+		if(result.hasErrors()) {
+			
+		    Map<String, String> errMap = new HashMap<>();
+		    
+		    for(FieldError error: result.getFieldErrors()) {
+		    	errMap.put(error.getField(), error.getDefaultMessage());
+		    }
+			
+			return new ResponseEntity<Map<String, String>>(errMap, HttpStatus.BAD_REQUEST);
+		}
+		
+		return null;
+	}
+}
